056606ffd6de54b182071ae4d6c711425ce45efd
KaylinAnn <40373229+KaylinAnn@users.noreply.github.com>
2019-04-08 15:53:11 -0700

    added root query and built schema in new file

--- server.js
+++ server.js
@@ -1,51 +1,17 @@
-var express = require('express')
-var graphqlHTTP = require('express-graphql')
-var { graphql, buildSchema } = require('graphql')
+const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const { graphql, buildSchema } = require('graphql')
+const schema = require('./schema/schema')
 
-var schema = buildSchema(`
-  type Query {
-    hello: String,
-    age: Int,
-    quoteOfTheDay: String,
-    rollThreeDice:[Int],
-    rollDice(numDice: Int!, numSides: Int): [Int]
-  }
-  `)
+const app = express()
 
-var root = {
-  hello: () => {
-    return 'Hello world!'
-  },
-  age: () => {
-    return 93
-  },
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
-  },
-  rollThreeDice: () => {
-    return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
-  },
-  rollDice: function ({ numDice, numSides }) {
-    var output = [];
-    for (var i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
-    }
-    return output;
-  }
-}
+app.use('/graphql', graphqlHTTP({
+  schema
+}))
 
-graphql(schema, '{hello, age, quoteOfTheDay, rollThreeDice, rollDice}', root).then(res => {
-  console.log(res);
+app.listen(4000, () => {
+  console.log('listening on port 4000');
 
 })
 
-var app = express()
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  rootValue: root,
-  graphiql: true
-}))
 
-app.listen(4000)
-
-console.log('Running Graphql API server at localhost:4000/graphql');
